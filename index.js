40a45d5aae0da7fe01ee539d675fe944fcfc16f1
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-04 00:26:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 $(document).ready(function (){
   document.getElementById('searchForm').addEventListener('submit', function(event){
     event.preventDefault();
-    console.log(event) ;
     var term = document.getElementById('searchTerms').value ;
     console.log(term) ;
   })
