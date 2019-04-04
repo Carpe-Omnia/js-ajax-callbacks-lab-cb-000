7c19850d7f76b0b6c2e0293d9b1f5e46c459f7c3
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-04 00:30:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ $(document).ready(function (){
     event.preventDefault();
     var term = document.getElementById('searchTerms').value ;
     console.log(term) ;
+    var base = `https://api.github.com/search/repositories?q=${term}`
   })
 });
