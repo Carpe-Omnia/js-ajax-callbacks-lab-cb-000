db2d22854819b0af8e5244f995f6c4bb84123b6c
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-04 01:00:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,14 +15,15 @@ $(document).ready(function (){
 });
 
 function htmlify(item){
-  var show = `<span onClick='showCommits()' > Show Commits </span>`
+  var show = `<span onClick='showCommits(${item.commits_url})' > Show Commits </span>`
   var thing = `<p>name: ${item.name} <br/> Description: ${item.description} <br/> <a href='${item.html_url}'> link </a> <br/> ${show} </p>` ;
   return thing ;
 }
-function showCommits(){
+function showCommits(url){
   //$.get(url, function(data){
     //console.log(data)
   //})
   console.log("showing commits")
+  console.log(url);
 }
 //${item.commits_url}
